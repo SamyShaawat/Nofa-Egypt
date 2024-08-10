43cb77b64235ff34b4f3a463a28607948af803b3
youssef essam <essamy@688gmail.com>
2024-08-10 15:33:19 +0300

    add product section

--- src/data/constants.js
+++ src/data/constants.js
@@ -2,47 +2,45 @@ export const navItems = [
   { path: "/", title: "Home" },
   { path: "/about-us", title: "About Us" },
   {
-    path: "/our-products", title: "Products", dropdown: true, subItems: [
+    path: "/our-products",
+    title: "Products",
+    dropdown: true,
+    subItems: [
       { path: "/our-products/solid-parquets", title: "Solid Parquets" },
       { path: "/our-products/mosaic-parquets", title: "Mosaic Parquets" },
       { path: "/our-products/laser-inlays", title: "Laser Inlays" },
       { path: "/our-products/outdoor-decking", title: "Outdoor Decking" },
       { path: "/our-products/garden-furniture", title: "Garden Furniture" },
-    ]
+    ],
   },
   { path: "/events-news", title: "Events & News" },
 ];
-export const contact = [
-
-  { path: "/contact-us", title: "Contact Us" },
-
-];
+export const contact = [{ path: "/contact-us", title: "Contact Us" }];
 
 export const carouselImages = [
-  '../images/carouselImages/img1.jpg', // replace with your image paths
-  '../images/carouselImages/img2.jpg',
-  '../images/carouselImages/img3.jpg',
-  '../images/carouselImages/img4.jpg',
-  '../images/carouselImages/img5.jpg',
-  '../images/carouselImages/img6.jpg',
+  "../images/carouselImages/img1.jpg", // replace with your image paths
+  "../images/carouselImages/img2.jpg",
+  "../images/carouselImages/img3.jpg",
+  "../images/carouselImages/img4.jpg",
+  "../images/carouselImages/img5.jpg",
+  "../images/carouselImages/img6.jpg",
 ];
 
 export const woodSpeciesImages = [
-  '../images/woodColors/Afromosia.png',
-  '../images/woodColors/Cherry.png',
-  '../images/woodColors/Doussie.png',
-  '../images/woodColors/Iroko.png',
-  '../images/woodColors/Mahogany.png',
-  '../images/woodColors/Padouk.png',
-  '../images/woodColors/Panga.png',
-  '../images/woodColors/Pine.png',
-  '../images/woodColors/RedOak.png',
-  '../images/woodColors/Sapelli.png',
-  '../images/woodColors/Tali.png',
-  '../images/woodColors/Teak.png',
-  '../images/woodColors/Walnut.png',
-  '../images/woodColors/Wenge.png',
-  '../images/woodColors/WhiteOak.png',
-  '../images/woodColors/YellowPine.png',
->>>>>>> origin/main
+  "../images/woodColors/Afromosia.png",
+  "../images/woodColors/Cherry.png",
+  "../images/woodColors/Doussie.png",
+  "../images/woodColors/Iroko.png",
+  "../images/woodColors/Mahogany.png",
+  "../images/woodColors/Padouk.png",
+  "../images/woodColors/Panga.png",
+  "../images/woodColors/Pine.png",
+  "../images/woodColors/RedOak.png",
+  "../images/woodColors/Sapelli.png",
+  "../images/woodColors/Tali.png",
+  "../images/woodColors/Teak.png",
+  "../images/woodColors/Walnut.png",
+  "../images/woodColors/Wenge.png",
+  "../images/woodColors/WhiteOak.png",
+  "../images/woodColors/YellowPine.png",
 ];
