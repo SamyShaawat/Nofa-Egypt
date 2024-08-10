d670d305d9241f289daeb950591c775d5cd5f6c4
SamyShaawat <samy.shaawat@gmail.com>
2024-08-10 14:23:24 +0300

    helloworld css

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,34 +9,33 @@ export default {
     extend: {
       screens: {
         'sm-max': { 'max': '639px' },
-        // => @media (max-width: 639px) { ... }
+        // => @media (max-width: 639px) 
 
         'md-max': { 'max': '767px' },
-        // => @media (max-width: 767px) { ... }
+        // => @media (max-width: 767px) 
 
         'lg-max': { 'max': '1023px' },
-        // => @media (max-width: 1023px) { ... }
+        // => @media (max-width: 1023px) 
 
         'xl-max': { 'max': '1279px' },
-        // => @media (max-width: 1279px) { ... }
+        // => @media (max-width: 1279px) 
 
         '2xl-max': { 'max': '1535px' },
-        // => @media (max-width: 1535px) { ... }
+        // => @media (max-width: 1535px) 
 
         'sm-min': { 'min': '640px' },
-        // => @media (min-width: 640px) { ... }
+        // => @media (min-width: 640px) 
 
         'md-min': { 'min': '768px' },
-        // => @media (min-width: 768px) { ... }
-
+        // => @media (min-width: 768px) 
         'lg-min': { 'min': '1024px' },
-        // => @media (min-width: 1024px) { ... }
+        // => @media (min-width: 1024px) 
 
         'xl-min': { 'min': '1280px' },
-        // => @media (min-width: 1280px) { ... }
+        // => @media (min-width: 1280px) 
 
         '2xl-min': { 'min': '1536px' },
-        // => @media (min-width: 1536px) { ... }
+        // => @media (min-width: 1536px) 
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"]
