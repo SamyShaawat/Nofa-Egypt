37780b5bb446900a6f31d53f7635190644f65bcc
SamyShaawat <samy.shaawat@gmail.com>
2024-08-24 18:01:34 +0300

    add events to news public page

--- src/pages/EventsNewsletter.jsx
+++ src/pages/EventsNewsletter.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import EventsHero from "../components/Events and Newsletter/EventsHero";
 import Newsletter from "../components/Events and Newsletter/Newsletter";
+import PublicEventsList from "../components/Events and Newsletter/PublicEventsList";
 
-const EventsNews = () => {
+const EventsNewsletter = () => {
   return (
     <>
       <Helmet>
@@ -11,8 +12,11 @@ const EventsNews = () => {
       </Helmet>
       <EventsHero />
       <Newsletter />
+      <div className="mt-8 mb-8">
+        <PublicEventsList />
+      </div>
     </>
   );
 };
 
-export default EventsNews;
+export default EventsNewsletter;
