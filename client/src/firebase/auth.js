a2a390bc162fb865d74b2e7d883880a84cbafefa
SamyShaawat <samy.shaawat@gmail.com>
2024-11-24 13:57:13 +0200

    add client and server folders

--- client/src/firebase/auth.js
+++
@@ -1,9 +0,0 @@
-import { auth } from "./firebase";
-import { signInWithEmailAndPassword } from "firebase/auth";
-
-export const doSignInWithEmailAndPassword = (email, password) => {
-    return signInWithEmailAndPassword(auth, email, password);
-};
-export const doSignOut = () => {
-    return auth.signOut();
-};
